eeab26da8568c4f644d926f7a54f3af00cf71e00
Jessica Garcia-Tejeda <jgarciat@trinity.edu>
2022-05-11 20:40:10 -0500

    Center Styling

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,11 @@
 function HomePage() {
   return (
     <div>
-      <h1>Welcome to Don't Whine About Wine!</h1>
+      <h1 align="center">Welcome to Don't Whine About Wine!</h1>
       <br></br>
+      <p align="center">
       <iframe src="https://drive.google.com/file/d/1l0Z9P6whateBDzLQAKMOWFbVBZ6R_UUQ/preview" width="640" height="480" allow="autoplay"></iframe>
+      </p>
       <br></br>
       <br></br>
       <br></br>
